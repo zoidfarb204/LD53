ac42a5852e4e0460c760e057a3366b263a73fabb
Mike Rosenberger <laxin204@gmail.com>
2023-05-06 22:46:47 -0500

    Working on pathfinding

---
+++ Assets/AStar.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Models;
+
+namespace DefaultNamespace
+{
+    public class AStar
+    {
+        private Queue<Node> path;
+
+        //given a starting node and ending node what is the fastest path between them
+        //use a priority queue to keep track of the nodes to visit
+        
+        
+        public Dictionary<Node,Node> Path(Node start, Node end)
+        {
+            path = new Queue<Node>();
+
+            var birdFlyDistance = Cost(start, end);
+            
+            var activeTiles = new List<Node>();
+            activeTiles.Add(start);
+            var visitedTiles = new Dictionary<Node,Node>();
+            
+            var g_score = new Dictionary<Node, double>();
+            g_score.Add(start, 0);
+
+            var f_score = new Dictionary<Node, double>();
+            f_score.Add(start, birdFlyDistance);
+            
+            while (activeTiles.Any())
+            {
+                var current = activeTiles.OrderBy(x => f_score[x]).First();
+                if(current.Id == end.Id)
+                {
+                    return visitedTiles;
+                }
+                
+                activeTiles.Remove(current);
+                foreach (var connectedNode in current.ConnectedNodes)
+                {
+                    if (!g_score.ContainsKey(connectedNode))
+                    {
+                        g_score.Add(connectedNode, double.MaxValue);
+                    }
+
+                    double tentative_g_score = g_score[current] + Cost(current, connectedNode);
+                    if(tentative_g_score < g_score[connectedNode])
+                    {
+                        visitedTiles[connectedNode] = current;
+
+                        g_score[connectedNode] = tentative_g_score;
+                        f_score[connectedNode] = g_score[connectedNode] + Cost(connectedNode, end);
+                        if (!activeTiles.Contains(connectedNode))
+                        {
+                            activeTiles.Add(connectedNode);
+                        }
+                    }
+                }
+            }
+
+            return null;
+        }
+        
+
+        private double Cost(Node node1, Node node2)
+        {
+            return Math.Sqrt(
+                Math.Pow((double)(node2.X - node1.X),2) 
+                + Math.Pow((double)(node2.Y - node1.Y),2));
+        }
+    }
+}
--- Assets/MapHandler.cs
+++ Assets/MapHandler.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Xml.Serialization;
+using DefaultNamespace;
 using Models;
 using UnityEngine;
 
@@ -13,62 +14,78 @@ public class MapHandler : MonoBehaviour
     {
         //read xml file into OsmXml class
         XmlSerializer xmlSerializer = new XmlSerializer(typeof(OsmXML.osm));
-        
+
         Debug.Log("Reading file...");
         FileStream fileStream = new FileStream("Assets/closer.osm", FileMode.Open);
         Debug.Log("Deserializing...");
-        OsmXML.osm osmXml = (OsmXML.osm) xmlSerializer.Deserialize(fileStream);
+        OsmXML.osm osmXml = (OsmXML.osm)xmlSerializer.Deserialize(fileStream);
         Debug.Log("Done");
 
         var latmin = osmXml.node.Min(x => x.lat);
         var latmax = osmXml.node.Max(x => x.lat);
         var longmin = osmXml.node.Min(x => x.lon);
         var longmax = osmXml.node.Max(x => x.lon);
-        var Nodes = new List<Node>();
+        
+        var nodeList = GenerateNodes(osmXml);
 
-        foreach (var way in osmXml.way)
+        foreach (var node in nodeList)
         {
-            //create an empty game object
-            GameObject go = new GameObject();
-            // add line renderer component
-            LineRenderer lr = go.AddComponent<LineRenderer>();
-            //set line segments to nd.count
+            var scaledY = (node.Y - latmin) / (latmax - latmin) * (200);
+            var scaledX = (node.X - longmin) / (longmax - longmin) * (200);
 
-            lr.positionCount = way.nd.Length;
-            lr.SetWidth(0.1f,.1f);
-            var count = 0;
-
-            foreach (var nd in way.nd)
+            foreach (var connectedNode in node.ConnectedNodes)
             {
-                var node = osmXml.node.FirstOrDefault(x => x.id == nd.@ref);
-                if (node == null)
-                {
-                    Debug.Log("Null node");
-                    continue;
-                }
-                if(node.lat == 0 || node.lon == 0)
-                    continue;
-                
-                //rescale x and y to fit in -100,100 based on the max and min values
-                var scaledY = (node.lat - latmin) / (latmax - latmin) * (200);
-                var scaledX = (node.lon - longmin) /(longmax - longmin) * (200);
-                //var scaledX = node.lat;
-                //var scaledY = node.lon;
-                lr.SetPosition(count, new Vector2((float)scaledX, (float)scaledY));
+                var connectedScaledY = (connectedNode.Y - latmin) / (latmax - latmin) * (200);
+                var connectedScaledX = (connectedNode.X - longmin) / (longmax - longmin) * (200);
+                //create an empty game object
+                GameObject go = new GameObject();
+                // add line renderer component
+                LineRenderer lr = go.AddComponent<LineRenderer>();
+                lr.material = new Material(Shader.Find("Legacy Shaders/Particles/Alpha Blended Premultiply"));
+                //set line segments to nd.count
+                lr.positionCount = 2;
+                lr.SetWidth(0.1f, .1f);
 
-                var n = new Node
-                {
-                    X = scaledX,
-                    Y = scaledY,
-                    ConnectedNodes = new List<Node>()
-                };
-                Nodes.Add(n);
-                count++;
+                lr.SetPosition(0, new Vector2((float)scaledX, (float)scaledY));
+                lr.SetPosition(1, new Vector2((float)connectedScaledX, (float)connectedScaledY));
             }
         }
 
+        //Pick 2 Random Nodes
+        var random = new System.Random();
+        var startNode = nodeList[random.Next(0, nodeList.Count)];
+        var endNode = nodeList[random.Next(0, nodeList.Count)];
+        //var startNode = nodeList.FirstOrDefault(x => x.Id == 7115700967);
+        //var endNode = nodeList.FirstOrDefault(x => x.Id == 1847104899);
+        Debug.Log($"Start Node: {startNode.Id} End Node: {endNode.Id}");
 
-        var nodeList = GenerateNodes(osmXml);
+        //Run pathfinding
+        var path = new AStar().Path(startNode, endNode);
+        if (path == null)
+        {
+            Debug.Log($"Start Node: {startNode.Id} End Node: {endNode.Id} -- could not find a path");
+        }
+
+        //Draw a straight line between the two nodes
+        //create an empty game object
+        var gameObject = new GameObject();
+        gameObject.name = "BirdPath";
+        // add line renderer component
+        LineRenderer lineRenderer = gameObject.AddComponent<LineRenderer>();
+        //set line segments to nd.count
+        lineRenderer.positionCount = 2;
+        lineRenderer.SetWidth(1f, 1f);
+        lineRenderer.material = new Material(Shader.Find("Legacy Shaders/Particles/Alpha Blended Premultiply"));
+        lineRenderer.startColor = Color.blue;
+        lineRenderer.endColor = Color.blue;
+        var x1 = (startNode.X - longmin) / (longmax - longmin) * (200);
+        var x2 = (endNode.X - longmin) / (longmax - longmin) * (200);
+        var y1 = (startNode.Y - latmin) / (latmax - latmin) * (200);
+        var y2 = (endNode.Y - latmin) / (latmax - latmin) * (200);
+        lineRenderer.SetPosition(0, new Vector2((float)x1, (float)y1));
+        lineRenderer.SetPosition(1, new Vector2((float)x2, (float)y2));
+
+        string s = "s";
     }
 
     private static List<Node> GenerateNodes(OsmXML.osm osmXml)
@@ -87,6 +104,12 @@ public class MapHandler : MonoBehaviour
 
         foreach (var way in osmXml.way)
         {
+            
+            if (way.tag == null || !IsRoadType(way.tag.FirstOrDefault(x => x.k == "highway")?.v))
+            {
+                continue;
+            }
+
             for (int i = 0; i < way.nd.Length; i++)
             {
                 var node = nodeList.FirstOrDefault(x => x.Id == way.nd[i].@ref);
@@ -107,7 +130,31 @@ public class MapHandler : MonoBehaviour
             }
         }
 
-        return nodeList.Where(x => x.Id != 0).ToList();
+        return nodeList.Where(x => x.Id != 0 && x.ConnectedNodes.Count > 0).ToList();
+    }
+
+    private static bool IsRoadType(string roadType)
+    {
+
+        if (string.IsNullOrEmpty(roadType))
+        {
+            return false;
+        }
+        
+        if (roadType == "motorway" ||
+            roadType == "residential" ||
+            roadType == "motorway_link" ||
+            roadType == "tertiary" ||
+            roadType == "secondary" ||
+            roadType == "unclassified" ||
+            roadType == "motorway" ||
+            roadType == "primary_link" ||
+            roadType == "primary")
+        {
+            return true;
+        }
+
+        return false;
     }
 
     // Update is called once per frame
